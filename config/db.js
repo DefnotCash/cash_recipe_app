f6acc0cbdf436cdb9fcdef0397f30fb6811cb14b
DefnotCash <kcash1316@gmail.com>
2022-10-04 13:35:09 +0100

    final change

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 export const connectDB = async() => {
     try {
         mongoose.connect("mongodb+srv://DefnotCash:<password>@cluster0.n9zuda2.mongodb.net/test")
-        console.log(`Connected to MongoDB Successfully`);
+        console.log(`Connected to MongoDB Successfully`)
     } catch (error) {
         console.error(`Error: ${error.message}`)
         process.exit(1)
